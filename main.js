4909c6cc531bf920e1cbc25e74ca6e4a1fb37d49
mammadkhan <90881574+mammadkhan@users.noreply.github.com>
2022-01-14 00:28:12 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-window.onload = map;
+const map = L.map('map').setView([40.43335959357837, 49.950714111328125], 11);
 
-function map(){
-    const map = new ol.Map({
-        view : new ol.View({
-            center : [0,0],
-            zoom : 2
-        }),
-        layers : [
-            new ol.layer.Tile({
-                source : new ol.source.OSM()
-            })
-        ],
-        target : 'js-map'
-    })
-}
+var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+});
+
+let circle = L.circle([40.43335959357837, 49.950714111328125],{
+    color:'red',
+    radius:800
+}).addTo(map);
+
+let circle2 = L.circle([40.46335959357837, 49.950714111328125],{
+    color:'red',
+    radius:500
+}).addTo(map);
+let circle3 = L.circle([40.44335959357837, 49.930714111328125],{
+    color:'red',
+    radius:500
+}).addTo(map);
+let circle4 = L.circle([40.42335959357837, 49.850714111328125],{
+    color:'red',
+    radius:500
+}).addTo(map);
+let circle5 = L.circle([40.38335959357837, 49.930714111328125],{
+    color:'red',
+    radius:500
+}).addTo(map);
+
+
+CartoDB_DarkMatter.addTo(map);
